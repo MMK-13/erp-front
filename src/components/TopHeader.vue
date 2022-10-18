f900d8c134a165b66dc8e56037c3aa71b926b078
MMK-13 <kantemamadou13@gmail.com>
2022-10-18 00:28:27 +0000

    Create TopHeader component and add it in DashboardView

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="header">
+		Top Header
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TopHeader',
+	}
+</script>
+
+<style scoped>
+	
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,18 +1,21 @@
 <template>
 	<LayoutContainer>
 		<SideBar />
+		<TopHeader />
 	</LayoutContainer>
 </template>
 
 <script>
-	import SideBar from '@/components/SideBar'
 	import LayoutContainer from '@/components/LayoutContainer'
+	import SideBar from '@/components/SideBar'
+	import TopHeader from '@/components/TopHeader'
 
 	export default {
 		name: 'DashboardView',
 		components: {
 			SideBar,
 			LayoutContainer,
+			TopHeader,
 		}
 	}
 </script>
