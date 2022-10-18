8a0bc4776fd98d1d89109cedc66d95bda23bd403
MMK-13 <kantemamadou13@gmail.com>
2022-10-18 00:55:41 +0000

    Stylizing TopHeader

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="header">
-		Top Header
+		<div class="header__left">
+			Left
+		</div>
+
+		<div class="header__center">
+			Center
+		</div>
+
+		<div class="header__right">
+			Right
+		</div>
 	</div>
 </template>
 
@@ -11,5 +21,16 @@
 </script>
 
 <style scoped>
-	
+	.header {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		padding: 0 16px;
+		color: #FFF;
+		border-bottom: 1px solid #152238;
+		border-left: 1px solid #152238;
+		background-color: #203354;
+		box-shadow: 2px 2px 8px #152238;
+	}
 </style>
