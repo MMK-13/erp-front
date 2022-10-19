91afd246ef7e7664f45178cc2f43193005dd78a3
MMK-13 <kantemamadou13@gmail.com>
2022-10-19 17:01:07 +0000

    Add new layout for centering components

---
+++ src/components/LayoutCenter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="container">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'LayoutCenter',
+	}
+</script>
+
+<style scoped>
+	.container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
