d61ebdf51fc0988b0650ba231f717e0021263128
MMK-13 <kantemamadou13@gmail.com>
2022-10-23 20:57:54 +0000

    Create BreadCrumb component add it in DashBoardView

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="breadcrumb">
+		<div class="breadcrumb__items">
+			<span v-for="(item, index) in exploded" :key="index" class="breadcrumb__items-item">
+				<i class="material-icons">chevron_right</i>
+				{{ item }}
+			</span>
+		</div>
+		<div class="breadcrumb__title">{{ title }}</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'BreadCrumb',
+		props: ['address', 'title'],
+		computed: {
+			exploded() {
+				return this.address.split('/')
+			},
+		},
+	}
+</script>
+
+<style scoped>
+	.breadcrumb {
+		color: #42B883;
+		margin-left: 32px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		grid-row-start: span 1;
+	}
+	.breadcrumb__items {
+		display: flex;
+		flex-direction: row;
+	}
+	.breadcrumb__items-item {
+		font-size: 16px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+	.breadcrumb__items-item i {
+		font-size: 24px;
+	}
+	.breadcrumb__title {
+		font-size: 22px;
+		margin-left: 24px;
+		font-weight: 500;
+	}
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,21 +1,16 @@
 <template>
-	<LayoutContainer>
-		<SideBar />
-		<TopHeader />
+	<LayoutContainer address="Home/Home page/Foo/Bar" title="Home Page">
+		<h2>Home page</h2>
 	</LayoutContainer>
 </template>
 
 <script>
 	import LayoutContainer from '@/components/LayoutContainer'
-	import SideBar from '@/components/SideBar'
-	import TopHeader from '@/components/TopHeader'
-
+	
 	export default {
 		name: 'DashboardView',
 		components: {
-			SideBar,
 			LayoutContainer,
-			TopHeader,
 		}
 	}
 </script>
