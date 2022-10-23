9411aecbbb0556ca9bb2d8faa07ce922c8e09828
MMK-13 <kantemamadou13@gmail.com>
2022-10-23 19:05:10 +0000

    Creation of main container

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="main">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'MainContainer',
+	}
+</script>
+
+<style>
+	.main {
+		margin: 32px;
+		padding: 16px;
+		border: 1px solid #EAEAEA;
+		background-color: #FFF;
+		border-radius: 4px;
+	}
+</style>
