c8e6b017fe0a105435c4eb322e0a22476ef5c58c
MMK-13 <kantemamadou13@gmail.com>
2022-10-28 02:51:27 +0000

    Creation of a transition component

---
+++ src/components/transitions/SlideUp.vue
@@ -0,0 +1,28 @@
+<template>
+	<transition name="slide-up">
+		<slot></slot>
+	</transition>
+</template>
+
+<script>
+	export default {
+		name: 'SlideUp',
+	}
+</script>
+
+<style scoped>
+	.slide-up-enter-active,
+	.slide-up-leave-active,
+	.slide-up-enter-active,
+	.slide-up-enter-active {
+		transition: all .3s ease-in;
+	}
+
+	.slide-up-enter-from,
+	.slide-up-leave-to,
+	.slide-up-enter-from,
+	.slide-up-leave-to {
+		transform: translateY(100px);
+		opacity: 0;
+	}
+</style>
