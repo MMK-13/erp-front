fd6386ff3eea1690fce9c14be641e11f195a21d9
MMK-13 <kantemamadou13@gmail.com>
2022-10-29 08:37:25 +0000

    Add notification data to vuex and get it in MainContainer

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,12 +1,42 @@
 <template>
 	<div class="main">
 		<slot></slot>
+		<AlertInfo
+			:type="notification.type"
+			:text="notification.text"
+			v-model:show="notification.show" />
 	</div>
 </template>
 
 <script>
+	import { mapGetters, mapActions } from 'vuex'
+	import AlertInfo from '@/components/forms/AlertInfo'
+
 	export default {
 		name: 'MainContainer',
+		components: {
+			AlertInfo,
+		},
+		mounted() {
+			this.notification = this.getNotification
+		},
+		data() {
+			return {
+				notification: {},
+			}
+		},
+		methods: {
+			...mapActions(['resetNotification']),
+		},
+		watch: {
+			notification(oldNotif, newNotif) {
+				if (!newNotif.show)
+					this.resetNotification()
+			}
+		},
+		computed: {
+			...mapGetters(['getNotification']),
+		},
 	}
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,19 @@ const state = {
 	user: {
 		isLogged: false,
 		data: {}
-	}
+	},
+	notification: {
+		show: true,
+		type: '',
+		text: ''
+	},
 }
 
 const getters = {
 	isConnected: (state) => state.user.isLogged,
 	userData: (state) => state.user.data,
 	fullname: (state) => state.user.data.firstname + ' ' + state.user.data.lastname,
+	getNotification: (state) => state.notification,
 }
 
 const mutations = {
@@ -20,6 +26,13 @@ const mutations = {
 	CONNECTED: (state) => {
 		state.user.isLogged = true
 	},
+	HELLO_MESSAGE: (state) => {
+		state.notification = {
+			show: true,
+			type: 'done',
+			text: 'Bienvenue ' + state.user.data.firstname + ' ' + state.user.data.lastname,
+		}
+	},
 	DISCONNECTED: (state) => {
 		state.user.isLogged = false
 		state.user.data = {}
@@ -27,15 +40,24 @@ const mutations = {
 	SET_USERNAME: (state, username) => {
 		state.user.data.username = username
 	},
+	RESET_NOTIFICATION: (state) => {
+		state.notification = {
+			show: false
+		}
+	}
 }
 
 const actions = {
 	setConnected: (store, data) => {
 		store.commit('CONNECTED')
 		store.commit('SET_USER_INFO', data)
+		store.commit('HELLO_MESSAGE')
 	},
 	setDisconnected: (store) => store.commit('DISCONNECTED'),
 	setUsername: (store, username) => store.commit('SET_USERNAME', username),
+	resetNotification: (store) => {
+		store.commit('RESET_NOTIFICATION')
+	},
 }
 
 export default createStore({
