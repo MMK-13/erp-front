ad86cfa37049ebdbe6d1a5c9cb41dad76fe061a7
MMK-13 <kantemamadou13@gmail.com>
2022-11-03 09:02:21 +0000

    Create a basic modal card and an basic overlay

---
+++ src/components/modal/BasicModal.vue
@@ -0,0 +1,95 @@
+<template>
+	<div class="basic-modal" v-if="isModalVisibille" @click="close">
+		<div class="modal" :style="{width: width}">
+			<div class="modal__header">
+				<slot name="header"></slot>
+				<i class="material-icons close-icons" @click="close">close</i>
+			</div>
+			<div class="modal__body">
+				<slot name="body"></slot>
+			</div>
+			<div class="modal__footer">
+				<slot name="footer"></slot>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { mapGetters, mapActions } from 'vuex'
+
+	export default {
+		name: 'BasicModal',
+		props: ['width'],
+		methods: {
+			...mapActions(['hideModal']),
+			close() {
+				this.hideModal()
+			},
+		},
+		computed: {
+			...mapGetters(['isModalVisibille']),
+		},
+	}
+</script>
+
+<style scoped>
+	.close-icons {
+		cursor: pointer;
+	}
+	.close-icons:hover {
+		transform: scale(1.1);
+	}
+	.basic-modal {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	.modal {
+		z-index: 102;
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		height: fit-content;
+		position: absolute;
+		top: 25%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.modal__header {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		font-size: 18px;
+		font-weight: 700;
+		border-top-left-radius: 4px;
+		border-top-right-radius: 4px;
+		border: 1px solid #152238;
+		background-color: #23395D;
+		color: #FFF;
+		padding: 8px 16px;
+	}
+	.modal__footer {
+		display: flex;
+		flex-direction: row;
+		gap: 8px;
+		font-size: 18px;
+		font-weight: 700;
+		border-bottom-left-radius: 4px;
+		border-bottom-right-radius: 4px;
+		border: 1px solid #152238;
+		background-color: #23395D;
+		color: #FFF;
+		padding: 8px 16px;
+	}
+	.modal__body {
+		background-color: #FFF;
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+		padding: 16px 32px;
+		border: 1px solid #152238;
+	}
+</style>
---
+++ src/components/modal/OverLay.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="overlay"
+		v-if="isModalVisibille"
+		@click="hide"></div>
+</template>
+
+<script>
+	import { mapGetters } from 'vuex'
+
+	export default {
+		name: 'OverLay',
+		computed: {
+			...mapGetters(['isModalVisibille']),
+		},
+	}
+</script>
+
+<style>
+	.overlay {
+		background-color: #000;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		opacity: .6;
+	}
+</style>
