d9dd32cdceaed429f9a4745609c4bdd03f6aaec0
MMK-13 <kantemamadou13@gmail.com>
2022-11-03 08:59:08 +0000

    Create Fade In oUt transition

---
+++ src/components/transitions/FadeInOut.vue
@@ -0,0 +1,22 @@
+<template>
+	<transition name="fade-in-out">
+		<slot></slot>
+	</transition>
+</template>
+
+<script>
+	export default {
+		name: 'FadeInOut',
+	}
+</script>
+
+<style scoped>
+	.fade-in-out-enter-active,
+	.fade-in-out-leave-active {
+		transition: all .3s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.fade-in-out-enter-from,
+	.fade-in-out-leave-to {
+		opacity: 0;
+	}
+</style>
