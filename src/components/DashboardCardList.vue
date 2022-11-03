a8f5859bbe48fcba684f8de43a1206c7d4fea1e9
MMK-13 <kantemamadou13@gmail.com>
2022-11-03 05:23:28 +0000

    Creation of a component to set Card component to show in Dashboard

---
+++ src/components/DashboardCardList.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="grid">
+		<DataCard v-for="(item, index) in items" :key="index"
+			:title="item.title"
+			:icon="item.icon"
+			:value="item.value"
+			:unity="item.unity" />
+	</div>
+</template>
+
+<script>
+	import DataCard from '@/components/visualization/DataCard'
+
+	export default {
+		name: 'DashboardCardList',
+		components: {
+			DataCard,
+		},
+		data() {
+			return {
+				items: [
+					{title: 'Ventes de la semaine', icon: 'attach_money', value: 100, unity: '₽'},
+					{title: 'Clients unique', icon: 'person', value: 54},
+					{title: 'Croissance', icon: 'trending_up', value: 11, unity: '%'},
+					{title: 'Mon solde', icon: 'account_balance', value: 871, unity: '₽'},
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-gap: 8px;
+	}
+</style>
