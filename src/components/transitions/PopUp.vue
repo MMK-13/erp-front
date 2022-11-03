bbc4d66569a16570c4444afc170d29136bbba0d4
MMK-13 <kantemamadou13@gmail.com>
2022-11-03 09:00:03 +0000

    Create popup transition

---
+++ src/components/transitions/PopUp.vue
@@ -0,0 +1,22 @@
+<template>
+	<transition name="pop-up">
+		<slot></slot>
+	</transition>
+</template>
+
+<script>
+	export default {
+		name: 'SlideUp',
+	}
+</script>
+
+<style scoped>
+	.pop-up-enter-active,
+	.pop-up-leave-active {
+		transition: all .3s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+	.pop-up-enter-from,
+	.pop-up-leave-to {
+		transform: scale(.5);
+	}
+</style>
